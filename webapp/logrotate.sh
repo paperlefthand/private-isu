df04f7f760eee1b6560ce570e0697fd42f4eed14
kosuke on earth <being.paperlefthand@gmail.com>
2024-03-24 20:48:54 +0900

    on earth

--- webapp/logrotate.sh
+++ webapp/logrotate.sh
@@ -2,3 +2,7 @@
 
 mv ./logs/nginx/access.log ./logs/nginx/access.log.`date +%Y%m%d-%H%M%S`
 docker compose exec nginx nginx -s reload
+
+sudo mv ./logs/mysql/mysql-slow.log ./logs/mysql/mysql-slow.log.`date +%Y%m%d-%H%M%S` 
+docker compose restart mysql 
+# docker compose exec mysql mysqladmin flush-logs

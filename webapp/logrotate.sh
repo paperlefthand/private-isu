b28e35b205035d953c23b6a5a207737d0d51c911
kosuke on earth <being.paperlefthand@gmail.com>
2024-07-13 09:10:38 +0900

    add k6 senarios

--- webapp/logrotate.sh
+++ webapp/logrotate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mv ./logs/nginx/access.log ./logs/nginx/access.log.`date +%Y%m%d-%H%M%S`
+sudo mv ./logs/nginx/access.log ./logs/nginx/access.log.`date +%Y%m%d-%H%M%S`
 docker compose exec nginx nginx -s reload
 
 sudo mv ./logs/mysql/mysql-slow.log ./logs/mysql/mysql-slow.log.`date +%Y%m%d-%H%M%S` 
